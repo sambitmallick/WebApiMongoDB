817844c07e5fe65dd587770e74bf61a08fdb31f0
Avinash Tiwari <avitiw@gmail.com>
2021-06-02 13:50:27 +0100

    Adding home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotebookAppApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Ok("OK");
+        }
+    }
+}
